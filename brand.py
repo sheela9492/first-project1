d23c0a832b0e2f5016ba6adafdb478080e33b2f5
sheela9492 <karpagasheela@outlook.com>
2024-09-17 13:51:56 +0100

    set brand name

---
+++ brand.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generatior.")
+city=input("What's the name of the city you grew up in?\n")
+pet=input("What's Your pet's name?\n")
+print("Your band name could be " + city + " " +pet)
