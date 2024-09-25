5e99c838af7451a7fbf262198f9c3f3a9a930ce9
sheela9492 <karpagasheela@outlook.com>
2024-09-25 11:22:38 +0100

    Update bill.py

---
+++ bill.py
@@ -0,0 +1,16 @@
+print("Welcome to the tip calculator!")
+bill=float(input("What was the total bill? $"))
+people=int(input("How many people to spilt the bill? "))
+total =bill/people
+print(total)
+score = 8
+score+=1
+print(score)
+print(round(33546.877554))
+print(round(5.7755, 2))
+age=29
+print(f"my age {age} years old")
+name = "sheela"
+age = 39
+is_ok= True
+print(f"My name {name}\nMy age{age}\nyou are{is_ok}")
